cbe69e4d2be704ac8cea1e4462f32e9af1baa7f0
James Fox-Collis <jlfoxcollis@gmail.com>
2020-10-20 09:27:09 -0700

    Iteration 3 Complete

--- lib/building.rb
+++ lib/building.rb
@@ -1,10 +1,11 @@
 class Building
-  attr_reader :units, :renters
+  attr_reader :units, :renters, :rent, :rented_units
 
   def initialize(units = [])
     @units = []
     @renters = []
-    @rent = 1099.5
+    @rent = rent
+    @rented_units = []
   end
 
   def add_unit(unit)
@@ -15,8 +16,28 @@ class Building
     @renters << renter
   end
 
-  def averate_rent
-    @rent
+  def rented_units
+    @units.select {|unit| unit.renter != nil}
   end
+
+  def average_rent
+    rent = units.map {|unit| unit.unit[:monthly_rent]}
+    average = (rent.sum.to_f / rent.count.to_f).round(1)
+  end
+
+  def renter_with_highest_rent
+    rent = rented_units.map {|unit| unit.unit[:monthly_rent]}
+    rent.max
+  end
+
+  def units_by_number_of_bedrooms
+    sorted = @units.sort {|a, b| b.unit[:bedrooms] <=> a.unit[:bedrooms]}
+    all_bedrooms = Hash.new { |hash_object, key| hash_object[key] = [] }
+    sorted.each do |unit|
+      all_bedrooms[unit.unit] << unit
+    end
+    all_bedrooms
+  end
+
 
 end
--- test/building_test.rb
+++ test/building_test.rb
@@ -24,7 +24,40 @@ class BuildingTest < MiniTest::Test
     assert_equal ["Aurora"], building.renters.map {|renter| renter.name}
     building.add_renter(renter2)
     assert_equal ["Aurora", "Tim"], building.renters.map {|renter| renter.name}
-    assert_equal 1099.5, building.averate_rent
+    assert_equal 1099.5, building.average_rent
+  end
+
+  def test_the_rent
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    unit3 = Apartment.new({number: "C3", monthly_rent: 1150, bathrooms: 2, bedrooms: 2})
+    unit4 = Apartment.new({number: "D4", monthly_rent: 1500, bathrooms: 2, bedrooms: 3})
+    renter1 = Renter.new("Spencer")
+    renter2 = Renter.new("Jessie")
+    renter3 = Renter.new("Max")
+
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    building.add_unit(unit3)
+
+    assert_equal [], building.rented_units
+
+    unit2.add_renter(renter1)
+    assert_equal "B2", building.rented_units[0].unit[:number]
+    assert_equal 999, building.renter_with_highest_rent
+
+    unit1.add_renter(renter2)
+    assert_equal 1200, building.renter_with_highest_rent
+
+    unit3.add_renter(renter3)
+    assert_equal 1200, building.renter_with_highest_rent
+
+
+
+    building.add_unit(unit4)
+    assert_equal ["D4", "B2", "C3", "A1"], building.units_by_number_of_bedrooms.keys.map {|x| x[:number]}
+
   end
 
 end
